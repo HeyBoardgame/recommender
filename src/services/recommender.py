cbccb0cfae413d25976232699defbe8934ca9e0f
s0o0bn <0921soobin@gmail.com>
2023-09-10 15:54:34 +0900

    🐛 추천 목록 생성 시 사용자 별 threshold 이상의 예측 평점인 아이템 목록으로 구성하도록 수정

--- src/services/recommender.py
+++ src/services/recommender.py
@@ -1,3 +1,5 @@
+import random
+
 from sqlalchemy.orm import Session
 
 from src.schemas.response import RecommendSchema
@@ -18,18 +20,22 @@ class RecommenderService:
     def __init__(self):
         self.model = load_pickled_model()
 
+    @staticmethod
+    def get_user_threshold(user_id: int, db: Session):
+        user_mean = rating.get_user_mean_rating(db, user_id)[0]
+        user_stddev = rating.get_user_stddev_rating(db, user_id)[0]
+        return user_mean + (user_stddev / 2)
+
     def recommend_to_single(self, user_id: int, db: Session):
+        threshold = self.get_user_threshold(user_id, db)
+
         rated_ids = rating.get_rated_board_games(db, user_id)
         unrated_board_games = boardgame.get_unrated_board_game_ids(db, rated_ids)
 
-        # user_id += self._user_id_base
+        user_id += self._user_id_base
         predictions = [self.model.predict(str(user_id), str(bg_id[0])) for bg_id in unrated_board_games]
-
-        def sort_key_est(pred):
-            return pred.est
-
-        predictions.sort(key=sort_key_est, reverse=True)
-        top_predictions = predictions[:self._rec_n]
+        over_threshold = [x for x in predictions if x.est >= threshold]
+        top_predictions = random.sample(over_threshold, 10)
 
         top_boardgame_ids = [int(float(pred.iid)) for pred in top_predictions]
         top_boardgame_score = [float(pred.est) for pred in top_predictions]
