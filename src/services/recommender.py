8df6161d0ae89c69ff47b444a6ff592810336c92
s0o0bn <0921soobin@gmail.com>
2023-09-19 15:02:03 +0900

    ✨ 개인 추천 시 보드게임 장르도 고려하도록 변경

--- src/services/recommender.py
+++ src/services/recommender.py
@@ -18,13 +18,13 @@ class RecommenderService:
         self.model = load_fm_model()
         self.num_recommendation = 10
 
-    def recommend_to_single(self, user_id: int, db: Session):
+    def recommend_by_genre(self, user_id: int, genre_id: int, db: Session):
         rated_ids = [ids[0] for ids in rating.get_rated_board_games(db, user_id)]
-        unrated_board_games = boardgame.get_unrated_board_games(db, rated_ids)
-        input_tensor = process_model_input(2522, unrated_board_games)
+        boardgame_with_genre = boardgame.get_unrated_board_games_by_genre(db, rated_ids, genre_id)
+        input_tensor = process_model_input(2522, boardgame_with_genre)
 
         predictions = self.model(input_tensor)
-        recommendable_ids = get_recommendable_items(unrated_board_games, predictions)
+        recommendable_ids = get_recommendable_items(boardgame_with_genre, predictions)
         sampled_recommendations = random.sample(recommendable_ids, self.num_recommendation)
 
         recommended_list = [int(bgg_id) for bgg_id in sampled_recommendations]
